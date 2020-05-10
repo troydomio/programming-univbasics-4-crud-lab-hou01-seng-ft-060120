9c7d00161707c71ea413309a600b3f061202eea7
Troy Domio <troyakins99@yahoo.com>
2020-05-10 05:28:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   wow = ["Wow", "I", "am", "really", "learning"]
-  wow << "Arrays!"
+  wow << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)

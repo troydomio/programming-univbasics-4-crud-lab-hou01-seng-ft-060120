5f88d447da3b1161fc2b6471877bafb1a40c0ef2
Troy Domio <troyakins99@yahoo.com>
2020-05-10 05:45:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array_subtract = []
+  array_subtract = ["I", "really", "am","learning","arrays"]
+  array_subtract.pop("arrays")
 end
 
 def remove_element_from_start_of_array(array)

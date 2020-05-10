cac3696ddeb04aeffb5e817a532def4b4593436f
Troy Domio <troyakins99@yahoo.com>
2020-05-10 05:51:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,8 +18,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array1 = ["I", "really", "am","learning","arrays"]
-  array1.delete("arrays")
+  array1 = ["I", "really", "am","learning","arrays!"]
+  array1.delete("arrays!")
 end
 
 def remove_element_from_start_of_array(array)

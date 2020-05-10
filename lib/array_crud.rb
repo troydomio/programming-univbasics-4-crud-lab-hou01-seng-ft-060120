72f6cd2255da29589422ec0a97757005dc3ff275
Troy Domio <troyakins99@yahoo.com>
2020-05-10 05:35:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array_add = ["I", "am", "really", "learning"]
+  array_add.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
